0a314426d067f927f3c1a20ef7b1efa171ea492b
alvesmaickel <maickel.l.alves@gmail.com>
2022-01-21 22:15:06 -0300

    sacrificio e dor, e sono!

--- server.js
+++ server.js
@@ -1,20 +1,36 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
 
 const app = express();
 
 app.use('/public', express.static('public'));
-
 app.use('/assets', express.static('assets'));
 
-app.get('/', function(req, res){
-    res.sendFile(__dirname +'/views/home.html');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/views/home.html');
 })
 
-app.get('/about', function(req, res){
-    res.sendFile(__dirname +'/views/about.html');
+app.get('/about', function(req, res) {
+    res.sendFile(__dirname + '/views/about.html');
+})
+
+app.post('/viacep', async function(req, res) {
+    const { cep } = req.body;
+
+    var result = await axios.get(`https://viacep.com.br/ws/${cep}/json/?callback`)
+        .then(res => res)
+        .catch(err => {
+            console.log('Error: ', err.message);
+        });
+
+    res.json(result.data);
 })
 
 //localhost:8080
-app.listen(8080, 'localhost', ()=> {
+app.listen(8080, 'localhost', () => {
     console.log('started')
 })
